326d223971d6a91f68da2ea76ad45cf12cc7a3fc
matthew <matthew.almeida@sbcglobal.net>
2023-12-13 14:23:45 -0500

    deployment

--- src/main/java/com/backend/configuration/SecurityConfiguration.java
+++ src/main/java/com/backend/configuration/SecurityConfiguration.java
@@ -104,12 +104,16 @@ public class SecurityConfiguration {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowCredentials(true);
-        configuration.setAllowedOrigins(Arrays.asList("https://localhost:3000", "https://cineverse-3smb.onrender.com", "https://cineverse-ualn.onrender.com"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
-        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+//        configuration.setAllowCredentials(true);
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+//        configuration.setAllowedOrigins(Arrays.asList("https://localhost:3000", "https://cineverse-3smb.onrender.com", "https://cineverse-ualn.onrender.com"));
+//        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
+//        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
+        System.out.println("CORS Configuration: " + configuration);
         return source;
     }
 }

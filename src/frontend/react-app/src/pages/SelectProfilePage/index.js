f89bc68c141fbdca066734b676441416661fb7fe
matthew <matthew.almeida@sbcglobal.net>
2023-12-14 18:37:53 -0500

    file structure

--- src/frontend/react-app/src/pages/SelectProfilePage/index.js
+++ src/frontend/react-app/src/pages/SelectProfilePage/index.js
@@ -21,7 +21,7 @@ function SelectProfilePage() {
                            <div>
                                <NavLink to="/browse">
                                    <div className="profile-icon">
-                                       <img src="https://occ-0-616-621.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABbGa4GvjA3sdbhrXZi7RG0-nuSXUxt-IZoVxB_7lHtMKT-wQ-CsDeukenQ6z6x4iUdqx4NJR4Sr3RDraWK1uYyKWRapH8T-tnFtb.png?r=59d"/>
+                                       <img src="https://occ-0-616-621.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABbGa4GvjA3sdbhrXZi7RG0-nuSXUxt-IZoVxB_7lHtMKT-wQ-CsDeukenQ6z6x4iUdqx4NJR4Sr3RDraWK1uYyKWRapH8T-tnFtb.png?r=59d" alt="profile icon"/>
                                    </div>
                                <span>
                                    {profile.name}

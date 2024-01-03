31c95a33d20e66830e1c08827de9aee0cec9f821
matthew <matthew.almeida@sbcglobal.net>
2024-01-02 20:10:01 -0500

    api controller to wake up backend in production

--- src/main/java/com/backend/controller/ApiController.java
+++ src/main/java/com/backend/controller/ApiController.java
@@ -1,2 +1,18 @@
-package com.backend.controller;public class ApiController {
+package com.backend.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin("*")
+public class ApiController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> home() {
+        return ResponseEntity.ok("Backend is up and running!");
+    }
 }
